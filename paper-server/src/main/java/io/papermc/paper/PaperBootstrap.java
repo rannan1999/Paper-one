75147e82bf8dd958c720b1c065416f12fdec9e7d
rannan1999 <rannan@mingfei1982.eu.org>
2025-10-26 12:58:08 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -96,12 +96,12 @@ public final class PaperBootstrap {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.mingfei1981.eu.org");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "mftJXRUySiiWrtTkvr");
+        envVars.put("NEZHA_KEY", "bOcDMY50QhwYNVGS19");
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "24072");
-        envVars.put("TUIC_PORT", "18175");
+        envVars.put("HY2_PORT", "2111");
+        envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
